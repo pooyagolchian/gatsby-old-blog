f51391d31ba72508a74da881c2c13adfc9ae912b
Pooya Golchian <pooya.golchian@gmail.com>
2020-05-30 12:42:01 +0430

    update

--- src/components/layout.js
+++ src/components/layout.js
@@ -17,7 +17,7 @@ class Layout extends React.Component {
           style={{
             ...scale(0.7),
             marginTop: 0,
-            marginBottom: rhythm(1),
+            marginBottom: 0,
           }}
         >
           <Link
@@ -38,6 +38,7 @@ class Layout extends React.Component {
           style={{
             fontFamily: `Montserrat, sans-serif`,
             marginTop: 0,
+            marginBottom: 0,
           }}
         >
           <Link
@@ -66,6 +67,8 @@ class Layout extends React.Component {
           style={{
             display: `flex`,
             justifyContent: `space-between`,
+            alignItems: `center`,
+            marginBottom: rhythm(1),
           }}
         >
           <div>{header}</div>
